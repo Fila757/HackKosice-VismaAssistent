2358e8f22e0567eab74cde16426515fd26b61ef4
fila <filip.cermak2@gmail.com>
2020-09-05 22:57:32 +0200

    stop printin iventiky

--- voice_recog/event_to_speech.py
+++ voice_recog/event_to_speech.py
@@ -1,6 +1,6 @@
 from voice_recog.text_to_speech import say
 
 def say_event(event):
-    print("printuju eventiky", event)
+    #print("printuju eventiky", event)
     say("Summary of the event is " + event['summary'] + " the description of the event is " + event['description'])
     say("Do you want to add this great event to your calendar?")
